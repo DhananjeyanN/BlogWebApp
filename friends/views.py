f5a33525bca857e98909867c09595981673e0419
dhananjeyannatarajan <potatoess1256@gmail.com>
2023-06-05 21:54:59 -0700

    profile updated functionality

---
+++ friends/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from friends.models import Friend
+
+
+# Create your views here.
+def add_friend(request, friend_id):
+    user = request.user
+    new_friend = User.objects.get(id=friend_id)
+    Friend.make_friend(user, new_friend)
+    return redirect('dashboard')
+
+def make_friend():
+    pass
+
+def friends(request):
+    friends = Friend.objects.all()
+    context = {'friends':friends}
+    return render(request, 'friends.html', context)
